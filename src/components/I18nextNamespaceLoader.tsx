0d2faa899c29021b791a61011a55cb3a545884d3
BLVD3 <00nvogel@gmail.com>
2024-03-24 19:32:28 +0100

    Added part of i18n loader component

---
+++ src/components/I18nextNamespaceLoader.tsx
@@ -0,0 +1,19 @@
+/*import { ReactNode, useEffect, useState } from "react"
+
+interface I18nextNameSpace {
+    namespace: string
+    children: ReactNode
+}
+
+function I18nextNamespaceLoader({namespace, children}:I18nextNameSpace) {
+    const [isNamespaceLoaded, setNamespaceLoaded] = useState(false);
+    useEffect(() => {
+
+    })
+
+    if (!isNamespaceLoaded) {
+        return <div>Loading...</div>;
+    }
+
+    return <>{children}</>
+}*/
