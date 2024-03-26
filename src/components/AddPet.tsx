384dbee34abfd9f5a0b196487066db8ae5c2b19b
BLVDE <00nvogel@gmail.com>
2024-03-26 15:11:29 +0100

    edited Pet model

---
+++ src/components/AddPet.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+
+const AddPet = () => {
+    const [page, setPage] = useState(0);
+    return (
+        <>
+        </>
+    )
+}
--- src/models/Pet.ts
+++ src/models/Pet.ts
@@ -37,3 +37,31 @@ const JSONPetToPet = (data: JSONPet) => {
         lastVetVisit: data.lastVetVisit ? new Date(data.lastVetVisit) : undefined
     } as Pet;
 }
+
+const PetToJSONPet = (data: Pet) => {
+    return {
+        ...data,
+        dateOfBirth: data.dateOfBirth ? data.dateOfBirth.toISOString() : undefined,
+        lastVetVisit: data.lastVetVisit ? data.lastVetVisit.toISOString() : undefined
+    }
+}
+
+const newPet = () : Pet => {
+    return {
+        id: 0,
+        name: "",
+        species: {
+            id: 0,
+            genus: {
+                id: 0,
+                name: "",
+            },
+            race: ""
+        },
+        owner: {
+            id: 0,
+            name: ""
+        },
+        isMale: false
+    }
+}
