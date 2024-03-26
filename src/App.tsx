7e33334e013c8cf31b7ef7fc27dada721152077e
Henri Staudenrausch <hstaudenra@stud.hs-heilbronn.de>
2024-03-26 17:00:51 +0100

    first header bar design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './components/css/App.css'
 import PetList from './components/PetList';
 import LoginInput from './components/LoginInput';
-import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import ErrorPage from './components/ErrorPage';
 import HeaderBar from './components/HeaderBar';
 
@@ -9,12 +9,12 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <HeaderBar site="login"/>
+        <HeaderBar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<PetList/>}/>
-            <Route path="/login"element={<LoginInput/>}/>
-            <Route path="*" element={<ErrorPage/>}/>
+            <Route path="/" element={<><PetList /><PetList /><PetList /><PetList /><PetList /></>} />
+            <Route path="/login" element={<LoginInput />} />
+            <Route path="*" element={<ErrorPage />} />
           </Routes>
         </div>
       </div>
--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -1,15 +1,56 @@
-import './css/App.css';
+import { useLocation } from 'react-router';
+import './css/HeaderBar.css';
+import { Link } from 'react-router-dom';
 
-const HeaderBar = ({site}:{site: string}) => {
+const HeaderBar = () => {
+    const location = useLocation();
     return (
-        <header className="App-header">
-            <div className="menu-icon">
-                <div className="menu-line"></div>
-                <div className="menu-line"></div>
-                <div className="menu-line"></div>
+        <div className="app-header">
+            <BurgerMenu />
+            <div className="site-title">
+                <h2>
+                    {(() => {
+                        switch (location.pathname) {
+                            case "/":
+                                return "Home";
+                            case "/login":
+                                return "Login";
+                            default:
+                                return "Error";
+                        }
+                    })()}
+                </h2>
             </div>
-            <h1>{site}</h1>
-        </header>
+            <div className="menu-items">
+                <MenuItem path="/" label="Home" />
+                <MenuItem path="/forum" label="Forum" />
+                <MenuItem path="/reminders" label="Reminders" />
+                <MenuItem path="/marketplace" label="Marketplace" />
+            </div>
+            <div className="user-menu">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+            </div>
+        </div>
+    )
+}
+
+function MenuItem({ path, label }: { path: string, label: string }) {
+    return (
+        <div className="menu-item">
+            <Link to={path}>{label}</Link>
+        </div>
+    )
+}
+
+function BurgerMenu() {
+    return (
+        <div className="menu-icon">
+            <div className="menu-line"></div>
+            <div className="menu-line"></div>
+            <div className="menu-line"></div>
+        </div>
     )
 }
 
