a4fa71063c050aa059ee62e92a3e44c525757832
BLVDE <00nvogel@gmail.com>
2024-03-26 10:38:26 +0100

    Added JSONPet

--- src/components/PetList.tsx
+++ src/components/PetList.tsx
@@ -3,17 +3,21 @@ import PetListItem from './PetListItem';
 import useFetch from '../hooks/useFetch';
 
 const PetList = () => {
-    const { data: pets, loading, error } = useFetch<Array<Pet>>('http://localhost:8000/pets');
-
+    const { data: data, loading, error } = useFetch<Array<JSONPet>>('http://localhost:8080/api/pets');
+    let pets : Array<Pet> = [];
+    if (data != null)
+        data.forEach(element => {
+            pets.push(JSONPetToPet(element))
+        });
     return (
         <div className="my-pets-module">
             <h2>Meine Tiere</h2>
             {loading && <div>Loading...</div>}
-            {!loading && !error && pets != null && <div className="pets">
+            {!loading && !error && data != null && <div className="pets">
                 {pets.map(pet => <PetListItem pet={pet} key={pet.id}></PetListItem>)}
             </div>}
             {!loading && error && <div>Error fetching pets</div>}
-            <svg className="add-pet-button" xmlns="http://www.w3.org/2000/svg" viewBox="-80 0 100 20" fill="currentColor">
+            <svg className="add-pet-button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm.75-11.25a.75.75 0 0 0-1.5 0v2.5h-2.5a.75.75 0 0 0 0 1.5h2.5v2.5a.75.75 0 0 0 1.5 0v-2.5h2.5a.75.75 0 0 0 0-1.5h-2.5v-2.5Z" />
             </svg>
         </div>
--- src/models/Pet.ts
+++ src/models/Pet.ts
@@ -13,3 +13,27 @@ interface Pet {
     favoriteToy?: string
     lastVetVisit?: Date
 }
+
+interface JSONPet {
+    id: number
+    name: string
+    species: Species
+    owner: User
+    dateOfBirth?: string
+    size?: number
+    weight?: number
+    castrated?: boolean
+    isMale: boolean
+    favoriteFood?: string
+    favoriteSpot?: string
+    favoriteToy?: string
+    lastVetVisit?: string
+}
+
+const JSONPetToPet = (data: JSONPet) => {
+    return {
+        ...data,
+        dateOfBirth: data.dateOfBirth ? new Date(data.dateOfBirth) : undefined,
+        lastVetVisit: data.lastVetVisit ? new Date(data.lastVetVisit) : undefined
+    } as Pet;
+}
