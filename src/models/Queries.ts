ce50e6375288bda49cff905717263ad4968b29d3
BLVDE <00nvogel@gmail.com>
2024-03-29 13:00:25 +0100

    Added Queries file

---
+++ src/models/Queries.ts
@@ -0,0 +1,40 @@
+import axios from "axios";
+import { Species } from "./Species"
+
+const axiosClient = axios.create({
+    baseURL: "http://localhost:8080/api",
+    headers: {
+        "Content-type": "application/json",
+    },
+})
+
+/**
+ * Request to get a species by id: "GET to /species/{id}"
+ * @param id ID of Species you want to fetch
+ * @returns Promise of requested Species
+ */
+const getSpeciesByID = (id: Number): Promise<Species> => {
+    return axiosClient.get(`/species/${id}`);
+}
+
+const updateSpecies = () => {
+
+}
+
+const deleteSpecies = () => {
+
+}
+
+const getAllSpecies = () => {
+
+}
+
+const postSpecies = () => {
+
+}
+
+
+
+export const MyPetQuery = {
+    
+}
