4b4586a40a65a5fa771e19f15db5674aad3d4b81
BLVDE <00nvogel@gmail.com>
2024-04-03 17:41:27 +0200

    Edited Base URL

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-export const API_BASE_URL = "http://localhost:8080/api";
+export const API_BASE_URL = "https://seserver.se.hs-heilbronn.de:8443/LabSWP24MyPet";
 
 export const APIClient = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: API_BASE_URL,
     headers: {
         "Content-type": "application/json",
     },
