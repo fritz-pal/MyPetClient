81f8789ba1111f6c147b1499882295a38d5576b8
BLVDE <00nvogel@gmail.com>
2024-04-04 10:29:01 +0200

    Added /api to url

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "https://seserver.se.hs-heilbronn.de:8443/LabSWP24MyPet";
+export const API_BASE_URL = "https://seserver.se.hs-heilbronn.de:8443/LabSWP24MyPet/api";
 
 export const APIClient = axios.create({
     baseURL: API_BASE_URL,
