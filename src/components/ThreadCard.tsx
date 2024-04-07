456b281a45ee417383e3bb95321d10fac3affb5b
Henri Staudenrausch <hstaudenra@stud.hs-heilbronn.de>
2024-04-07 22:12:39 +0200

    modified ThreadCard

--- src/components/ThreadCard.tsx
+++ src/components/ThreadCard.tsx
@@ -1,13 +1,17 @@
+import { Thread } from "../models/Forum";
+import './css/ForumPage.css';
+
 const ThreadCard = ({ thread }: { thread: Thread }) => (
-    <>
-        <div className="pet-tile" key={pet.name}>
-            <img className="pet-image" src={petImage} alt={pet.name} />
-            <div className="pet-info">
-                <div className="pet-name">{pet.name}</div>
-                <div className="pet-type">{pet.species.name}</div>
-            </div>
-            <button className="view-pet-button">→</button>
+    <div className="thread-card" >
+        <div className="thread-info">
+            <div className="thread-timestamp">{thread.createdAt}</div>
+            <div className="thread-species">{thread.species.name}</div>
         </div>
-        <hr />
-    </>
+        <div className="thread-creator">{thread.creator?.username}</div>
+        <div className="thread-title">
+            <div className="thread-name">{thread.name}</div>
+            <div className="thread-description">{thread.description}</div>
+        </div>
+        <button className="view-thread-button">→</button>
+    </div>
 );
