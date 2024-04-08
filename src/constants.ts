b4c18d0fa2d0ef92823dcc65f55bd2e1c3bd8951
BLVDE <00nvogel@gmail.com>
2024-04-08 10:19:57 +0200

    Fixed Thread post

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export const API_BASE_URL = "https://seserver.se.hs-heilbronn.de:8443/LabSWP24MyPet/api";
+//export const API_BASE_URL = "http://localhost:8080/api";
 
 export const APIClient = axios.create({
     baseURL: API_BASE_URL,
