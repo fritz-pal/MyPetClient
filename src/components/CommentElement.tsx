d2a0b358e3aa29ae57bbb08f0d4dc7a49ee8c99b
Jannik Danecker <danecker.jannik@gmail.com>
2024-04-18 17:36:40 +0200

    remove textures

--- src/components/CommentElement.tsx
+++ src/components/CommentElement.tsx
@@ -7,11 +7,13 @@ import { useParams } from "react-router";
 import { useTranslation } from "react-i18next";
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
+import { useState } from 'react';
 
 
 const CommentElement = ({comment} : {comment: Comment}) => {
 const [t,] = useTranslation("comment");
 const { user } = useContext(UserContext); 
+const [clicked, setClicked] = useState(false);
 
     const queryClient = useQueryClient();
     const { id } = useParams();
@@ -25,6 +27,14 @@ const { user } = useContext(UserContext);
             })
         }
     })
+
+    const handleClick = () => {
+        if (clicked) {
+          deleteComment(comment.id);
+        } else {
+          setClicked(true);
+        }
+     };
     
     const deleteComment = (id: number) => {
         console.log(`deleteComment called with id: ${id}`);
@@ -36,7 +46,9 @@ const { user } = useContext(UserContext);
             <div className="comment-body">
                 {comment.text.split("\n").map((line) => {return line != "" ? <div className="comment-line">{line}</div> : <br/>})}
                 {comment.poster.id == user.id ?
-                <button className="deleteButton-Comment" onClick={() => {deleteComment(comment.id);}}>{t("delete")}</button>: ""}
+                <button className={`deleteButton-Comment ${clicked ? 'confirm' : ''}`}  onClick={handleClick}>
+                {clicked ? t("confirm") : t("delete")}
+              </button>: ""}
             </div>
         </div>
     )
