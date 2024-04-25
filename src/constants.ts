654692988621ea52235979883539c005c23b11e9
BLVDE <00nvogel@gmail.com>
2024-04-25 21:16:11 +0200

    Changed apiclient base url

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "https://seserver.se.hs-heilbronn.de:8443/LabSWP24MyPet/api";
-//export const API_BASE_URL = "http://localhost:8080/api";
+export const API_BASE_URL = process.env.NODE_ENV === 'production' ? "/api" : "http://localhost:8080/api";
 
 export const APIClient = axios.create({
     withCredentials: true,
