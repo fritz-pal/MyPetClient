df26dd518e6beee09dd5ce0ffa2500706c714b0c
BLVDE <00nvogel@gmail.com>
2024-05-13 13:58:40 +0200

    Fixed Bug when opening answer

--- src/components/PosterInfo.tsx
+++ src/components/PosterInfo.tsx
@@ -4,20 +4,21 @@ import "./css/PosterInfo.css"
 import { timeSince } from "../util/time";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router-dom";
+import { Button } from "react-aria-components";
 
 const PosterInfo = ({poster, postedAt}: {poster: User, postedAt?: number}) => {
     const [t,] = useTranslation("time")
     const navigate = useNavigate();
-    const handleClick = () => {
+    const openUser = () => {
         navigate("/user/" + poster.id);
     }
 
     return (
-        <div className="poster-info" onClick={handleClick}>
+        <Button className="poster-info" onPress={openUser}>
             <img className="poster-info-image" src={noIcon} />
             { poster.username }
             { postedAt ? <> • { timeSince(postedAt, t) }</> : <></> }
-        </div>
+        </Button>
     );
 }
 
