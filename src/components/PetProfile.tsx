cabf0f46422e385c7b394c840d215a804e040b2c
Jannik Danecker <danecker.jannik@gmail.com>
2024-05-16 15:03:16 +0200

    Zwischenstand User Storie

--- src/components/PetProfile.tsx
+++ src/components/PetProfile.tsx
@@ -1,14 +1,33 @@
 import { useParams } from "react-router"
 import { PetAPI } from "../models/Pet";
 import { useQuery } from "@tanstack/react-query";
+import RoundImage from "./RoundImage";
+import placeholderPet from '/placeholderPet.png';
+import './css/PetProfile.css';
+import { useState } from "react";
+import { ReminderAPI } from "../models/Reminder";
+import Loader from "./Loader";
+
 
 const PetProfile = () => {
+    const [isJumping, setIsJumping] = useState(false);
+
+    const handleClick = () => {
+        if (!isJumping) {
+            setIsJumping(true);
+            setTimeout(() => setIsJumping(false), 500); // Animation dauert 0.5s
+        }
+    };
     const { id } = useParams();
-    console.log(id);
 
     const petQuery = useQuery({
         queryKey: ["pets", id],
         queryFn: () => PetAPI.getPetByID(id ? Number(id) : 0)
+    });
+
+    const petReminderQuery = useQuery({
+        queryKey: ["pet-reminders", id],
+        queryFn: () => ReminderAPI.getReminderByPetID(Number(id))
     });
 
     if (petQuery.isLoading) {
@@ -20,9 +39,34 @@ const PetProfile = () => {
     }
 
     return (
-        <div>
-            <h1>{petQuery.data?.name}</h1>
-            <h2>{petQuery.data?.dateOfBirth}</h2>
+        <div className="pet-profile-container">
+            <div>
+                <div className="pet-profile">{petQuery.data?.name}</div>
+                <RoundImage className={`pet-profile-image ${isJumping ? 'jump' : ''}`} imageSource={petQuery.data?.imageSource} placeholder={placeholderPet} />
+                <button className="test-button-jump" onClick={handleClick}>HEY</button>
+                <div className="pet-profile">{petQuery.data?.dateOfBirth}</div>
+                <div className="pet-profile">{petQuery.data?.id}</div>
+                <div className="pet-profile">{petQuery.data?.castrated ? "Kastriert" : "nicht Kastriert"}</div>
+                <div className="pet-profile">{petQuery.data?.owner.username}</div>
+                <div className="pet-profile">{petQuery.data?.isMale ? "Männlich" : "Weiblich"}</div>
+            </div>
+            <div>
+                <div className="pet-profile">Reminder</div>
+                {petReminderQuery.isLoading && (
+                    <div className="load">
+                        <Loader />
+                    </div>
+                )}
+                {petReminderQuery.isSuccess && (
+                    <div className="reminders">
+                        {petReminderQuery.data.map((reminder) => (
+                            console.log(reminder.id),
+                            console.log(reminder.date),
+                            <div className="reminder-name">{reminder.name}</div>
+                        ))}
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
