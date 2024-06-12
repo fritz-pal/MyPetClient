04fe8ec27116c195ee0dfdce58d6f9d5717de483
BLVDE <00nvogel@gmail.com>
2024-06-12 22:04:06 +0200

    Chats in a List react to messages now

--- src/components/ChatListElement.tsx
+++ src/components/ChatListElement.tsx
@@ -4,10 +4,14 @@ import { UserContext } from "../context/UserContext";
 import "./css/ChatListElement.css"
 import { usePress } from "react-aria";
 import { useNavigate } from "react-router";
+import useStomp from "../hooks/useStomp";
 
 const ChatListElement = ({chat} : {chat: ChatRoom}) => {
     const {user} = useContext(UserContext);
     const nav = useNavigate();
+    const [lastMessage, setLastMessage] = useState(chat.lastMessage ? chat.lastMessage : null);
+
+    useStomp("/messages/" + chat.id, (message) => setLastMessage(JSON.parse(message)))
 
     const [otherUser] = useState(getOtherUserInChat(chat, user));
 
@@ -20,9 +24,9 @@ const ChatListElement = ({chat} : {chat: ChatRoom}) => {
     return (
         <div {...pressProps} className="chat-list-element">
             <div className="chat-list-element-title">{otherUser.username}</div>
-            <div className="chat-list-element-message-preview">{chat.lastMessage?.text}</div>
-            {chat.lastMessage && (() => {
-                const date = new Date(chat.lastMessage.createdAt);
+            <div className="chat-list-element-message-preview">{lastMessage?.text}</div>
+            {lastMessage && (() => {
+                const date = new Date(lastMessage.createdAt);
                 return (
                     <div className="chat-list-element-timestamp">
                         {`${date.toLocaleDateString()} ${date.toLocaleTimeString()}`}
