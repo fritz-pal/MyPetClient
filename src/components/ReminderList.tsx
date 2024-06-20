33ad1aa56c8e5e635128e7a268bf4c954fed1b82
Jannik Danecker <danecker.jannik@gmail.com>
2024-06-20 12:21:48 +0200

    reminder verschiebung fix

--- src/components/ReminderList.tsx
+++ src/components/ReminderList.tsx
@@ -139,7 +139,7 @@ const ReminderListItem = ({ reminder }: { reminder: Reminder }) => {
         return result;
     }
 
-    function formatDateTime(dateString: string, locale: string, reminderName: string): string {
+    function formatDateTime(dateString: string, locale: string): string {
         const options: Intl.DateTimeFormatOptions = {
             weekday: "long",
             day: "2-digit",
@@ -149,10 +149,6 @@ const ReminderListItem = ({ reminder }: { reminder: Reminder }) => {
             minute: "2-digit",
         };
         const date = new Date(dateString);
-        if (reminderName !== "Birthday") {
-            const offset = date.getTimezoneOffset();
-            date.setMinutes(date.getMinutes() - offset)
-        }
         const formattedDate = date
             .toLocaleDateString(locale, options)
             .replace(",", "");
@@ -174,7 +170,7 @@ const ReminderListItem = ({ reminder }: { reminder: Reminder }) => {
             <div className="reminder-area" key={reminder.name}>
                 <div className="reminder-info"></div>
                 <div className="reminder-date">
-                    {formatDateTime(reminder.date.toString(), currentLocale, reminder.name)}
+                    {formatDateTime(reminder.date.toString(), currentLocale)}
                 </div>
                 <div className="reminder-pets-container">
                     {reminder.pets.map((pet) => (
